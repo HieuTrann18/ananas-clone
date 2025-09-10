f8cb4f28976344b9d0c035d2bb7bdf61615ba006
TranTheHieu <hieu.tt.2455@aptechlearning.edu.vn>
2025-09-10 14:30:15 +0700

    Delete frontend directory

--- frontend/src/components/Header/Header.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { FaBox } from 'react-icons/fa';
-import './Header.css'
-const Header = () => {
-  return <div>this is header</div>
-};
-
-export default Header;
--- frontend/src/pages/Home.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Header from '../components/Header/Header';
-const Home = () => {
-  return <div>fuck home</div>;
-};
-
-export default Home;
